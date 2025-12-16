10dee11be52ccd0a13a129d875c1527b88678152
Vlad Yaroshuk <vlad.yaroshuck@gmail.com>
2025-12-16 17:20:22 +0200

    refactor: restructure main loop using functions and match/case

--- concession stand.py
+++ concession stand.py
@@ -11,61 +11,63 @@ menu = {"pizza" : 3.00,
 
 cart = []
 total = 0
-help_mode = False
-remove_mode = False
+
+def add_item(order):
+    price = menu.get(order)
+    cart.append(order)
+    global total
+    total += price
+    print(f"\nAdded {order} for ${price:.2f}. Current total: ${total:.2f}") 
 
 def current_cart():
-    if total > 0:
+    if cart:
         print("\nYour shopping cart:")
-        for cart_item in cart:
-            print(f"- {cart_item}: ${menu.get(cart_item):.2f}")
+        for item in cart:
+            print(f"- {item}: ${menu.get(item):.2f}")
         print(f"Current total: ${total:.2f}")
     else:
-        print("Your shopping cart is empty!")
+        print("\nYour shopping cart is empty!")
+        
+def remove_loop():
+    while True:
+        item_to_remove = input("\nEnter the name of the item to remove ('q' to quit remove mode): ").lower()
+        if item_to_remove != "q":
+            remove_item(item_to_remove)
+        else:
+            break
+        
+def remove_item(item_to_remove):
+    if item_to_remove in cart:
+        removed_price = menu.get(item_to_remove)
+        cart.remove(item_to_remove)
+        global total
+        total -= removed_price
+        print(f"\nRemoved {item_to_remove} for ${removed_price:.2f}. Current total: ${total:.2f}")
+    else:
+        print(f"\n{item_to_remove} is not in your cart.")
 
 print("----CINEMA BAR----")
 for item, price in menu.items():
     print(f"{item:8} : ${price:.2f}")
 print("------------------")
 
-while not help_mode: 
-    order = input("\nWhat do you wanna buy? ('help' for other options, 'done' to finish): ").lower()
-    if order == 'help':
-        help_mode = True
-        while help_mode:
+while True: 
+    order = input("\nWhat do you wanna buy? ('check' to see current cart, 'remove' to remove items, 'done' to finish): ").lower()
+    match order:
+        case _ if order in menu:
+            add_item(order)
+        case 'remove':
             current_cart()
-            command = input("\nYou can 'remove' to remove items, 'back' to continue shopping and 'done' to finish: ").lower()
-            if command == 'remove':  
-                remove_mode = True
-                while remove_mode:
-                    item_to_remove = input("Enter the name of the item to remove ('q' to quit remove mode): ").lower()
-                    if item_to_remove in cart:
-                        index = cart.index(item_to_remove)
-                        removed_price = menu.get(item_to_remove)
-                        cart.pop(index)
-                        total -= removed_price
-                        print(f"\nRemoved {item_to_remove} for ${removed_price:.2f}. Current total: ${total:.2f}")
-                        current_cart()
-                    elif item_to_remove == "q":
-                        remove_mode = False
-                    else:
-                        print(f"Sorry, {item_to_remove} is not in cart. Try again!")
-            elif command == 'back':
-                help_mode = False
-            elif command == 'done':
-                break        
-    elif order == 'done':
-        break
-    elif order in menu:
-        price = menu.get(order)
-        cart.append(order)
-        total += price
-        print(f"Added {order} for ${price:.2f}. Current total: ${total:.2f}") 
-    else:
-        print(f"Sorry, {order} is not in menu. Try again.")
+            remove_loop()
+        case 'check':
+            current_cart()
+        case 'done':
+            break
+        case _:
+            print(f"\nSorry, {order} is not in menu. Try again.")
         
-if total > 0:
-    current_cart()  
-    print("Thank you for buying!")
+current_cart()  
+if cart:
+    print("\nThank you for buying!")
 else:
-    print("Thanks for coming! Enjoy your time.")
+    print("\nThanks for coming! Enjoy your time.")
